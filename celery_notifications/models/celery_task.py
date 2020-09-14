38fd1d5d543abf51c38376bc3ca1f05153e051ba
Antonio Buric <antonio.burich@gmail.com>
2020-09-14 06:14:29 +0200

    [UPD] Migrated to v13.

--- celery_notifications/models/celery_task.py
+++ celery_notifications/models/celery_task.py
@@ -1,7 +1,7 @@
 # Copyright Otrium (http://www.otrium.nl)
 
 from odoo import api, fields, models, _
-from odoo.addons.celery.models.celery_task import STATE_JAMMED, STATE_FAILURE, STATE_SUCCESS
+from odoo.addons.celery.models.celery_task import STATE_FAILURE, STATE_SUCCESS
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -16,7 +16,6 @@ class CeleryTask(models.Model):
         Methods:
          * _get_signup_url_for_action from res.partner
         """
-        self.ensure_one()
         url = self.env.user.partner_id.sudo().with_context(
             signup_valid=True,
         )._get_signup_url_for_action(
@@ -33,7 +32,7 @@ class CeleryTask(models.Model):
         if 'state' in vals:
             mail_server = self.env['ir.mail_server']
             for task in self:
-                if task.state in [STATE_JAMMED, STATE_FAILURE, STATE_SUCCESS]:
+                if task.state in [STATE_FAILURE, STATE_SUCCESS]:
                     notify_settings = self.env['celery.task.setting'].search([('model', '=', task.model), ('method', '=', task.method)])
                     notification = {
                         'title': 'Celery Task',
@@ -49,25 +48,6 @@ class CeleryTask(models.Model):
                                 'text': 'View Tasks',
                                 'icon': 'fa-cog'}],
                     }
-                    if task.state == STATE_JAMMED and notify_settings and notify_settings.jammed_notify_users_ids:
-                        try:
-                            notification.update(title='Celery Jammed Task', timeout=notify_settings.jammed_notification_duration * 1000)
-                            notify_settings.jammed_notify_users_ids._notify_action(notification)
-                            # email
-                            if notify_settings.jammed_send_email_notification and notify_settings.jammed_send_email_template:
-                                template = notify_settings.jammed_send_email_template
-                                for user in notify_settings.jammed_notify_users_ids:
-                                    body_html = template._render_template(template.body_html, 'celery.task', task and task.id)
-                                    message = mail_server.build_email(
-                                        email_from=self.sudo().env.user.email,
-                                        subject=_(u'Jammed Celery Task'),
-                                        body=body_html,
-                                        subtype='html',
-                                        email_to=[user.partner_id.email],
-                                    )
-                                    mail_server.send_email(message)
-                        except Exception as e:
-                            _logger.warning('Celery Notification Error: %s' % (str(e), ))
                     if task.state == STATE_FAILURE and notify_settings and notify_settings.failed_notify_users_ids:
                         try:
                             notification.update(title='Celery Failed Task', icon='fa fa-3x fa-times', timeout=notify_settings.failed_notification_duration * 1000)
@@ -116,15 +96,14 @@ class CeleryTask(models.Model):
             setting = self.env['celery.task.setting'].browse(celery_task_setting_id)
             if setting and setting.send_task_digest_email and setting.message_follower_ids:
                 order_by_field = 'state_date'
-                jammed_tasks = self.search([('state', '=', STATE_JAMMED), ('model', '=', setting.model), ('method', '=', setting.method), ('sent_with_mail_digest', '=', False)], order=order_by_field)
                 failed_tasks = self.search([('state', '=', STATE_FAILURE), ('model', '=', setting.model), ('method', '=', setting.method), ('sent_with_mail_digest', '=', False)], order=order_by_field)
 
-                if jammed_tasks or failed_tasks:
+                if failed_tasks:
                     template = self.env.ref('celery_notifications.celery_task_digest_template', raise_if_not_found=False)
                     if template:
-                        body_html = template.with_context(jammed_tasks=jammed_tasks, failed_tasks=failed_tasks)._render_template(template.body_html, 'celery.task.setting', setting.id)
+                        body_html = template.with_context(jammed_tasks=[], failed_tasks=failed_tasks)._render_template(template.body_html, 'celery.task.setting', setting.id)
                         mail_server = self.env['ir.mail_server']
-                        message = mail_server.with_context(jammed_tasks=jammed_tasks, failed_tasks=failed_tasks).build_email(
+                        message = mail_server.with_context(jammed_tasks=[], failed_tasks=failed_tasks).build_email(
                             email_from=self.sudo().env.user.email,
                             subject=_(u'Celery Tasks Digest'),
                             body=body_html,
@@ -132,5 +111,4 @@ class CeleryTask(models.Model):
                             email_to=[follower.partner_id.email for follower in setting.message_follower_ids],
                         )
                         mail_server.send_email(message)
-                        jammed_tasks.write({'sent_with_mail_digest': True})
                         failed_tasks.write({'sent_with_mail_digest': True})
--- celery_notifications/models/celery_task_setting.py
+++ celery_notifications/models/celery_task_setting.py
@@ -27,23 +27,6 @@ class CeleryTaskSetting(models.Model):
         if template:
             return template.id
 
-    jammed_notify_users_ids = fields.Many2many(
-        string="Notify users (jammed)",
-        comodel_name="res.users",
-        relation="celery_task_setting_res_users_jammed_rel",
-        column1="task_id",
-        column2="user_id",
-        default=_default_user_ids,
-        help="Users to receive a Web/E-mail notification when these tasks are jammed.",
-        track_visibility='onchange',
-    )
-    jammed_send_email_notification = fields.Boolean(string="Send Instant E-mail notification (jammed)", default=False)
-    jammed_send_email_template = fields.Many2one('mail.template', string="Mail template (jammed)",
-                                                 help="Use this mail template for jammed task e-mails.", default=_default_email_template)
-    jammed_are_you_inside = fields.Boolean(string='Are you inside the matrix? (jammed)',
-                                           compute='_are_you_inside', store=False, readonly=True)
-    jammed_notification_duration = fields.Integer(string="Notification Duration for Jammed Tasks (seconds)", default=5)
-
     failed_notify_users_ids = fields.Many2many(
         string="Notify users (failed)",
         comodel_name="res.users",
@@ -85,16 +68,13 @@ class CeleryTaskSetting(models.Model):
     ], string='E-mail Digest interval type', default='hours', required=True, track_visibility='onchange')
 
     def _are_you_inside(self):
-        self.jammed_are_you_inside = bool(self.env.uid in [u.id for u in self.jammed_notify_users_ids])
         self.failed_are_you_inside = bool(self.env.uid in [u.id for u in self.failed_notify_users_ids])
         self.success_are_you_inside = bool(self.env.uid in [u.id for u in self.success_notify_users_ids])
 
     def action_join(self):
         self.ensure_one()
         state_change = self.env.context.get('state_change', False)
-        if state_change and state_change == 'jammed':
-            return self.write({'jammed_notify_users_ids': [(4, self._uid)]})
-        elif state_change and state_change == 'failed':
+        if state_change and state_change == 'failed':
             return self.write({'failed_notify_users_ids': [(4, self._uid)]})
         elif state_change and state_change == 'success':
             return self.write({'success_notify_users_ids': [(4, self._uid)]})
@@ -102,9 +82,7 @@ class CeleryTaskSetting(models.Model):
     def action_quit(self):
         self.ensure_one()
         state_change = self.env.context.get('state_change', False)
-        if state_change and state_change == 'jammed':
-            return self.write({'jammed_notify_users_ids': [(3, self._uid)]})
-        elif state_change and state_change == 'failed':
+        if state_change and state_change == 'failed':
             return self.write({'failed_notify_users_ids': [(3, self._uid)]})
         elif state_change and state_change == 'success':
             return self.write({'success_notify_users_ids': [(3, self._uid)]})
